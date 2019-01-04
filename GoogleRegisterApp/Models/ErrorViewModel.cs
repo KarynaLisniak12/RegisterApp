a7b4f9ed71cdb3d1a74409e18611129f39641fc8
Karyna <karyna.lisniak@gmail.com>
2019-01-04 13:29:40 +0200

    Initial

---
+++ GoogleRegisterApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoogleRegisterApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
